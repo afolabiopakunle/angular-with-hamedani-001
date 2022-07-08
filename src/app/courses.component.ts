a6fd01a5c405756268ce59e09cdfd812281f3fb9
afolabiopakunle <opakunleafolabi@gmail.com>
2022-07-09 00:32:29 +0100

    ngFor directive implemented

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,9 +3,15 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-courses',
-  template: '<h2>Courses</h2><p>My courses</p>'
+  template: `<h2>Courses</h2>
+  <p>My courses</p>
+  <ul>
+    <li *ngFor="let course of courses">{{ course }}</li>
+  </ul>
+  `
 })
 
 export class CoursesComponent {
 
+  courses = ['course1', 'course2', 'course3']
 }
